229b44f261f12a75d1d1619c552bd139585632cb
gyoritamas <gyoritamasnoe@gmail.com>
2022-02-21 09:43:25 +0100

    Create Teacher entity

---
+++ src/main/java/com/codecool/gradebookapi/model/Teacher.java
@@ -0,0 +1,28 @@
+package com.codecool.gradebookapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    public String getName() {
+        return firstname + " " + lastname;
+    }
+}
