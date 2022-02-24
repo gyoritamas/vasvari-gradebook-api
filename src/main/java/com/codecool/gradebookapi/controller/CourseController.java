aba24e4300a5b9bca523d495b2fe321a1675ecbc
gyoritamas <gyoritamasnoe@gmail.com>
2022-02-24 07:48:23 +0100

    Create TeacherController

--- src/main/java/com/codecool/gradebookapi/controller/CourseController.java
+++ src/main/java/com/codecool/gradebookapi/controller/CourseController.java
@@ -6,9 +6,11 @@ import com.codecool.gradebookapi.dto.assembler.CourseModelAssembler;
 import com.codecool.gradebookapi.exception.CourseInUseException;
 import com.codecool.gradebookapi.exception.CourseNotFoundException;
 import com.codecool.gradebookapi.exception.StudentNotFoundException;
+import com.codecool.gradebookapi.exception.TeacherNotFoundException;
 import com.codecool.gradebookapi.service.CourseService;
 import com.codecool.gradebookapi.service.GradebookService;
 import com.codecool.gradebookapi.service.StudentService;
+import com.codecool.gradebookapi.service.TeacherService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -26,7 +28,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/classes")
 @Slf4j
-@Tag(name = "class-controller", description = "Operations on classes")
+@Tag(name = "class-controller", description = "Operations on courses")
 public class CourseController {
 
     @Autowired
@@ -36,33 +38,36 @@ public class CourseController {
     private StudentService studentService;
 
     @Autowired
+    private TeacherService teacherService;
+
+    @Autowired
     private GradebookService gradebookService;
 
     @Autowired
     private CourseModelAssembler assembler;
 
     @GetMapping
-    @Operation(summary = "Lists all classes")
-    @ApiResponse(responseCode = "200", description = "Returned list of all classes")
+    @Operation(summary = "Lists all courses")
+    @ApiResponse(responseCode = "200", description = "Returned list of all courses")
     public ResponseEntity<CollectionModel<EntityModel<CourseOutput>>> getAll() {
-        log.info("Returned list of all classes");
+        log.info("Returned list of all courses");
 
         return ResponseEntity
                 .ok(assembler.toCollectionModel(courseService.findAll()));
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Finds a class by its ID")
+    @Operation(summary = "Finds a course by its ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Returned class with given ID"),
-            @ApiResponse(responseCode = "404", description = "Could not find class with given ID")
+            @ApiResponse(responseCode = "200", description = "Returned course with given ID"),
+            @ApiResponse(responseCode = "404", description = "Could not find course with given ID")
     })
     public ResponseEntity<EntityModel<CourseOutput>> getById(@PathVariable("id") Long id) {
-        CourseOutput classFound = courseService.findById(id).orElseThrow(() -> new CourseNotFoundException(id));
-        log.info("Returned class {}", id);
+        CourseOutput courseFound = courseService.findById(id).orElseThrow(() -> new CourseNotFoundException(id));
+        log.info("Returned course {}", id);
 
         return ResponseEntity
-                .ok(assembler.toModel(classFound));
+                .ok(assembler.toModel(courseFound));
     }
 
     @PostMapping
@@ -82,35 +87,51 @@ public class CourseController {
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Updates the class given by ID")
+    @Operation(summary = "Updates the course given by ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Updated class with given ID"),
-            @ApiResponse(responseCode = "400", description = "Could not update class due to invalid parameters"),
-            @ApiResponse(responseCode = "404", description = "Could not find class with given ID")
+            @ApiResponse(responseCode = "200", description = "Updated course with given ID"),
+            @ApiResponse(responseCode = "400", description = "Could not update course due to invalid parameters"),
+            @ApiResponse(responseCode = "404", description = "Could not find course with given ID")
     })
-    public ResponseEntity<EntityModel<CourseOutput>> update(@RequestBody @Valid CourseInput clazz,
+    public ResponseEntity<EntityModel<CourseOutput>> update(@RequestBody @Valid CourseInput course,
                                                             @PathVariable("id") Long id) {
         courseService.findById(id).orElseThrow(() -> new CourseNotFoundException(id));
-        log.info("Updated class {}", id);
+        log.info("Updated course {}", id);
 
         return ResponseEntity
-                .ok(assembler.toModel(courseService.update(id, clazz)));
+                .ok(assembler.toModel(courseService.update(id, course)));
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Deletes the class given by ID")
+    @Operation(summary = "Deletes the course given by ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Deleted class with given ID"),
-            @ApiResponse(responseCode = "404", description = "Could not find class with given ID"),
-            @ApiResponse(responseCode = "405", description = "Could not delete class with given ID")
+            @ApiResponse(responseCode = "204", description = "Deleted course with given ID"),
+            @ApiResponse(responseCode = "404", description = "Could not find course with given ID"),
+            @ApiResponse(responseCode = "405", description = "Could not delete course with given ID")
     })
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         courseService.findById(id).orElseThrow(() -> new CourseNotFoundException(id));
         if (!gradebookService.findByClassId(id).isEmpty()) throw new CourseInUseException(id);
         courseService.deleteById(id);
-        log.info("Deleted class {}", id);
+        log.info("Deleted course {}", id);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{courseId}/set_teacher/{teacherId}")
+    @Operation(summary = "Set the teacher of the course")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Set the teacher of the course"),
+            @ApiResponse(responseCode = "404", description = "Could not find course/teacher with given ID")
+    })
+    public ResponseEntity<EntityModel<CourseOutput>> setTeacherOfCourse(@PathVariable("courseId") Long courseId,
+                                                                        @PathVariable("teacherId") Long teacherId) {
+        courseService.findById(courseId).orElseThrow(() -> new CourseNotFoundException(courseId));
+        teacherService.findById(teacherId).orElseThrow(() -> new TeacherNotFoundException(teacherId));
+        log.info("Set teacher {} to class {}", teacherId, courseId);
+
+        return ResponseEntity
+                .ok(assembler.toModel(courseService.setTeacherOfCourse(teacherId, courseId)));
     }
 
     @PostMapping("/{courseId}/class_enrollment/{studentId}")
@@ -123,7 +144,7 @@ public class CourseController {
                                                                        @PathVariable("studentId") Long studentId) {
         courseService.findById(courseId).orElseThrow(() -> new CourseNotFoundException(courseId));
         studentService.findById(studentId).orElseThrow(() -> new StudentNotFoundException(studentId));
-        log.info("Added student {} to class {}", studentId, courseId);
+        log.info("Added student {} to course {}", studentId, courseId);
 
         return ResponseEntity
                 .ok(assembler.toModel(courseService.addStudentToCourse(studentId, courseId)));
