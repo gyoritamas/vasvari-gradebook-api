9756852bf544a64fce5e399c12bc928edbcb9109
gyoritamas <gyoritamasnoe@gmail.com>
2022-03-04 19:54:03 +0100

    Configure Spring Security JWT Authentication

--- src/main/java/com/codecool/gradebookapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/codecool/gradebookapi/security/ApplicationSecurityConfig.java
@@ -1,99 +1,33 @@
 package com.codecool.gradebookapi.security;
 
 import com.codecool.gradebookapi.auth.ApplicationUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codecool.gradebookapi.jwt.JwtAuthenticationEntryPoint;
+import com.codecool.gradebookapi.jwt.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.concurrent.TimeUnit;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.codecool.gradebookapi.security.ApplicationUserRole.ADMIN;
 import static com.codecool.gradebookapi.security.ApplicationUserRole.TEACHER;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtAuthenticationEntryPoint entryPoint;
+    private final ApplicationUserService userService;
+    private final JwtRequestFilter requestFilter;
     private final PasswordEncoder passwordEncoder;
-    private final ApplicationUserService applicationUserService;
-
-    @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
-                                     ApplicationUserService applicationUserService) {
-        this.passwordEncoder = passwordEncoder;
-        this.applicationUserService = applicationUserService;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-//                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))  // handle unauthorized attempts
-//                .and()
-//                .headers().frameOptions().disable()     // to enable h2-console
-//                .and()
-                .authorizeRequests()
-                    .antMatchers(
-                        "/",
-                        "index",
-                        "/css/*",
-                        "/js/*",
-                        "/h2-console/*"
-                    ).permitAll()
-                    .antMatchers(
-                        "/csrf",
-                        "/v2/api-docs",
-                        "/swagger-resources/**",
-                        "/swagger-ui.html",
-                        "/webjars/**"
-                    ).permitAll()       // for swagger UI
-                    .antMatchers(
-                        "/api/users/**",
-                        "/api/students/**",
-                        "/api/teachers/**",
-                        "/api/classes/**"
-                    ).hasRole(ADMIN.name())
-                    .antMatchers(
-                        "/api/students/**",
-                        "/api/assignments/**",
-                        "/api/gradebook/**",
-                        "/api/student_gradebook/**",
-                        "/api/class_gradebook/**"
-                    ).hasRole(TEACHER.name())
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .failureUrl("/courses")
-                    .defaultSuccessUrl("/swagger-ui.html", true)
-                    .usernameParameter("username")  // these have to match the name parameters
-                    .passwordParameter("password")  // in the login form
-                    .and()
-                .rememberMe()
-                    .userDetailsService(applicationUserService)     // need this to work!!!
-                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(20)) // overriding default 2 weeks expiration
-                    .key("something-very-secure")   // hash key
-                //.tokenRepository()    ----> use database instead of in-memory storage
-                    .rememberMeParameter("remember-me")     // if not set, default password, username, remember-me are used
-                    .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()))  // should use POST instead of GET if csrf is NOT disabled
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID", "remember-me")
-                    .logoutSuccessUrl("/login");
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -106,12 +40,36 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("student").password(passwordEncoder.encode("student")).roles("STUDENT");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/students/**").hasAnyRole(ADMIN.name(), TEACHER.name())
+                .antMatchers("/api/teachers/**").hasAnyRole(ADMIN.name())
+                // Permit all other request without authentication
+                .and().authorizeRequests().anyRequest().permitAll()
+                // Reject every unauthenticated request and send error code 401.
+                .and().exceptionHandling().authenticationEntryPoint(entryPoint)
+                // We don't need sessions to be created.
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Add a filter to validate the tokens with every request
+        http.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(applicationUserService);
+        provider.setUserDetailsService(userService);
 
         return provider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
