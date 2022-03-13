a70a7d000711fda6902b324fe53c8db5e170cf1b
gyoritamas <gyoritamasnoe@gmail.com>
2022-03-13 10:13:02 +0100

    Remove user permissions

--- src/main/java/com/codecool/gradebookapi/security/ApplicationUserRole.java
+++ src/main/java/com/codecool/gradebookapi/security/ApplicationUserRole.java
@@ -4,33 +4,11 @@ import com.google.common.collect.Sets;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import static com.codecool.gradebookapi.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
-    // these are the permissions certain roles have
-    ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE)),
-    TEACHER(Sets.newHashSet(STUDENT_READ, COURSE_READ)),
-    STUDENT(Sets.newHashSet());
-
-    private final Set<ApplicationUserPermission> permissions;
-
-    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public Set<ApplicationUserPermission> getPermissions() {
-        return permissions;
-    }
+    ADMIN, TEACHER, STUDENT;
 
     public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
-        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toSet());
-
-        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-
-        return permissions;
+        return Sets.newHashSet(new SimpleGrantedAuthority("ROLE_" + this.name()));
     }
 }
