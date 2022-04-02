8ec3d712461da592735122155dac486cc8aa20b8
gyoritamas <gyoritamasnoe@gmail.com>
2022-04-02 15:12:06 +0200

    Add constraints to username and password

--- src/main/java/com/codecool/gradebookapi/dto/dataTypes/UsernameInput.java
+++ src/main/java/com/codecool/gradebookapi/dto/dataTypes/UsernameInput.java
@@ -6,14 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class UsernameInput {
-    @NotBlank(message = "Username field cannot be empty")
+
     @Schema(example = "johndoe91")
+    @Size(min = 4, max = 20)
+    @Pattern(regexp = "^[a-zA-Z]([0-9a-zA-Z]){3,20}",
+            message = "username can only contain letters and numbers and must start with a letter")
     private String username;
 }
--- src/main/java/com/codecool/gradebookapi/model/request/PasswordChangeRequest.java
+++ src/main/java/com/codecool/gradebookapi/model/request/PasswordChangeRequest.java
@@ -5,11 +5,20 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 public class PasswordChangeRequest {
     private String oldPassword;
+
+    @Size(min = 8, max = 20)
+    @Pattern(regexp = "^(?=.*[0-9])"
+            + "(?=.*[a-z])(?=.*[A-Z])"
+            + "(?=\\S+$).{8,20}$",
+            message = "password must contain a number, a lower- and an uppercase letter")
     private String newPassword;
 }
